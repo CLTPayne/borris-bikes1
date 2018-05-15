9ba2b52e835d293a0fcf9c8ce78215968907b55f
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-16 00:48:05 +0100

    Write and pass first Rspec example

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,33 @@
 require "docking_station"
 
 describe DockingStation do
-
+  it { is_expected.to respond_to :release_bike }
 end
+
+=begin
+# If not using the one-liner syntax ??
+describe DockingStation do
+  it "responds when asked to release_bike" do
+    expect(subject).to respond_to :release_bike
+  end
+end
+
+But why is the method a symbol?
+And why is subject used for the test instead of being specfic about how you want to run it.
+E.g. the storing names test, we gave an example name.
+Hence why not have an example class
+
+can you not do:
+describe DockingStation do
+  it { is_expected.to respond_to(release_bike) }
+end
+
+OR
+
+describe DockingStation do
+  it "responds when asked to release the bike" do
+    expect(docking_station).to respond_to(release_bike)
+  end
+end
+
+=end
