7dd7045688e13659b3ac7471bd000e16c2a80bbb
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-16 15:06:05 +0100

    Step 10 - release a bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,11 @@
+require_relative 'bike'
+
 class DockingStation
 
   def release_bike
-    
+    Bike.new
+    # output is currently nil
+    # need a bike as the output
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,33 +1,10 @@
-require "docking_station"
+require "./lib/docking_station"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-end
 
-=begin
-# If not using the one-liner syntax ??
-describe DockingStation do
-  it "responds when asked to release_bike" do
-    expect(subject).to respond_to :release_bike
+  it "releasing a working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
-
-But why is the method a symbol?
-And why is subject used for the test instead of being specfic about how you want to run it.
-E.g. the storing names test, we gave an example name.
-Hence why not have an example class
-
-can you not do:
-describe DockingStation do
-  it { is_expected.to respond_to(release_bike) }
-end
-
-OR
-
-describe DockingStation do
-  it "responds when asked to release the bike" do
-    expect(docking_station).to respond_to(release_bike)
-  end
-end
-
-=end
