90de9d6a4926b856e29701bfae89f6588fc1a488
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-16 17:39:14 +0100

    Step 13 - Dock too full fail

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,11 +5,13 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    fail 'No bikes available' unless @bike
+    fail "No bikes available" unless @bike
     @bike
   end
 
   def dock(bike)
+    fail "Docking station full" if @bike
+    # Use if here as it's the opposite of unless (e.g. !@bike)
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 require "./lib/docking_station"
 
+# In this challenge, you will raise an exception when a user attempts to dock a bike at a DockingStation that already contains a bike (i.e. with a capacity of 1). Scaffolding has been removed, but the steps are similar to Challenge 12
+
 describe DockingStation do
   describe '#release_bike' do
     it 'releases a bike' do
@@ -10,22 +12,26 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
-    it 'raises an error when there a no bikes available' do
+    it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
-  
-  it 'docks something' do
-    bike = Bike.new
-
-    expect(subject.dock(bike)).to eq bike
   end
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    it 'docks a bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+  describe '#dock' do
+    it 'raises an error when there are too many bikes in the docking station' do
+      subject.dock(Bike.new)
+      expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
+    end
   end
 
-
-  end
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
 end
