43ddee97ae8d81986debca1aff5a5527a06b759b
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-05-17 22:12:25 +0100

    Step 14 - docking station is a collection of 20 bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,35 +1,30 @@
 require "./lib/docking_station"
 
 describe DockingStation do
+  
   describe '#release_bike' do
-    it 'releases a bike' do
+
+    it 'releases a bike from the group of bikes' do
       bike = Bike.new
-      subject.dock(bike)
+      20.times { subject.dock(bike) }
       expect(subject.release_bike).to eq bike
     end
-  end
 
-  describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
 
-    it 'docks a bike' do
-      bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
-    end
-
    describe '#dock' do
     it 'raises an error when there are too many bikes in the docking station' do
-    #  20.times { subject.dock(Bike.new) }
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
     end
   end
 
     it 'returns docked bikes' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      # is this still a working test? Have we just hard coded the last
+      expect(subject.dock(bike)[-1]).to eq bike
     end
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,4 +3,4 @@ require './lib/bike.rb'
 
 docking_station = DockingStation.new
 bike = Bike.new
-20.times { docking_station.dock(bike ) }
+docking_station.full?
